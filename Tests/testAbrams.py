ca0c080d5b2dbd1fc2634ef7c69aa726ac83d227
Serhii Sychov <sychov.s@gmail.com>
2023-06-12 21:18:15 +0300

    update test abrams

--- Tests/testAbrams.py
+++ Tests/testAbrams.py
@@ -3,20 +3,29 @@
 
 def testAbrams():
         print('Бажаєте пройти наш тест?')    
-        print('Напишіть так, або ні')                            
-        tank=[1980, 'Нарізна', 105, 'Глаткоствольна', 'М1 Абрамс', 120, 1500, 72, 66, 4]
-        test=input()
-        if(test=='так'):
+        print('Напишіть так, або ні')    
+        #list                        
+        true_answer_list = [1980, 'Нарізна', 105, 'Глаткоствольна',
+                             'М1 Абрамс', 120, 1500, 72, 66, 4]
+        
+        user_answer = input()
+
+        if(user_answer.lower() == 'так'):
                 print('Добре, тоді продовжемо')
-        elif(test=='ні'):
+        elif(user_answer.lower() == 'ні'):
                  print('Добре, допобачення')
+                 return
         else:
                 print('Я вас не розумію')
+                return
         _bal=0
-        print('Назвіть основний бойовий танк Америки?')
+
+        print('Назвіть основний бойовий танк США?')
         print('а)М1 Абрамс б)Леопард 2 А4 в)Челенджер 2 г)Т 90')
-        tank=input()
-        if(tank=='М1 Абрамс'):
+        
+        user_answer = input()
+
+        if(user_answer.lower() == true_answer_list[0].lower()):
                 print('Відповідь вірна')
                 _bal=_bal+1
         else:
@@ -24,61 +33,72 @@ def testAbrams():
 
         print('З якого року почалась серійне виробництво Абрамсів?')
         print('2000 1980 2010 1990')
-        tank=int(input())
-        if(tank==1980):
+        
+        user_answer = int(input())
+
+        if(user_answer == true_answer_list[1]):
                 print('Відповідь вірна') 
                 _bal=_bal+2
         else:
                 print('Відповідь не вірна')
+
         print('Зі скількох людей складаєтся екіпаж Абрамса?')
         print('2  3  4  5')
-        tank=int(input())
-        if(tank==4):
+        
+        user_answer = int(input())
+        if(user_answer == true_answer_list[3]):
                 print('Відповідь вірна') 
                 _bal=_bal+1
         else:
                 print('Відповідь не вірна')
         print('Який тип гармати встановлений на Абрамсі?')
         print('Глаткоствольна, Нарізна, Полігональна Нарізна')
-        tank=input()
-        if(tank=='Нарізна'):
+        
+        user_answer = int(input())
+        if(user_answer == true_answer_list[4]):
                 print('Відповідь вірна') 
                 _bal=_bal+1
-        elif(tank=='Глаткоствольна'):
+        elif(user_answer == true_answer_list[5]):
                 print('Починаючи з версії М1А1 встановлена Глаткоствольна гармата.')
                 _bal=_bal+1
         else:
                 print('Відповідь не вірна')
+
         print('Який калібр гармати Абрамса (мм)?')
         print('120  105  100  150')
-        tank=int(input())
-        if(tank==105):
+
+        user_answer = int(input())
+        if(user_answer == true_answer_list[6]):
                 print('Відповідь вірна') 
                 _bal=_bal+1
-        elif(tank==120):
+        elif(user_answer == true_answer_list[7]):
                 print('Починаючи з версії М1А1 встановлена 120 міліметрова гармата.')
                 _bal=_bal+1
         else:
                 print('Відповідь не вірна')
         print('Яка потужність двигуна Абрамс(к.с.)?')
         print('1500  1000  1100  2000')
-        tank=int(input())
-        if(tank==1500):
+        
+        user_answer = int(input())
+        if(user_answer == true_answer_list[8]):
                 print('Відповідь вірна') 
                 _bal=_bal+2
         else:
                 print('Відповідь не вірна')      
+        
         print('Яка швидкість по шосе Абрамса (км/год?)')
         print('72 100 66 50')
-        tank=int(input())
-        if(tank==72):
+        
+        user_answer = int(input())
+        if(user_answer == true_answer_list[9]):
                 print('Відповідь вірна') 
                 _bal=_bal+3
-        elif(tank==66):
+        elif(user_answer == true_answer_list[10]):
                 print('Починаючи з версії М1А1 розвиває швидкість по шосе 66,8 км/год.')
                 _bal=_bal+3
         else:
                 print('Відповідь не вірна') 
 
         print('Ваш результат '+ str(_bal) + ' балів')
+
 testAbrams()
--- examples/Lists.py
+++ examples/Lists.py
@@ -8,16 +8,27 @@ list = [1,2,3,4,5]
 print(list)
 
 #оголошення списку тварин
-animals = ['dog', 'cat', 'snake', 'bird']
+animals = ['DoG', 'cat', 'snake', 'bird']
 print(animals)
 #додавання до списку нового елементу
 animals.append('elephant')
 
+#доступ до елементів списку через індекс, починаючи з нуля
+print(animals[0])
+print(animals[1])
+print(animals[2])
+print(animals[3])
+
 #оголошення іншого списку тварин
 myAnimals = ['monkey', 'perot']
-
-print(animals)
 print(myAnimals)
+
+#вставка елемента в список
+myAnimals.insert(1, 'dog')
+print(myAnimals)
+
+myAnimals.append('dog')
+
 #розшрирення одного списку іншим
 animals.extend(myAnimals)
 
