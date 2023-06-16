676bb7e2f6ddaa757eddc234ff623ba4f9624842
misha-tovstenko <tovstenko.misha@gmail.com>
2023-06-16 18:42:59 +0300

    WhileList

--- WhileList.py
+++ WhileList.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3 
 
 classmates=['Влад', 'Саша', 'Максим', 'Рома', 'Андрій', 'Юра', 'Артем', 'Вова' ]
-clas=0
 name=0
-while(clas<8):
-    clas=clas+1
+while(name<8):
     print(classmates[name])
     name=name+1
     
