ac9b999dec57ea6cf788295daecf9358af685dfb
misha-tovstenko <tovstenko.misha@gmail.com>
2023-06-16 17:58:53 +0300

    WhileList

--- Tests/cosmos.py
+++ Tests/cosmos.py
@@ -5,7 +5,7 @@ def cosmos():
     print('Бажаєте пройти наш тест?')    
 
     print('Напишіть так, або ні')                            
-    cosmos={8,9, 82,5, 4, 13}
+    
     test=input()
 
     if(test=='так'):
@@ -17,54 +17,54 @@ def cosmos():
     _bal=0
     print('Скільки планет у сонячній системі?')
     print('1   4   8   9')
-    cosmos=int(input())
-    if(cosmos==8):
+    plenet=int(input())
+    if(plenet==8):
         print('Відповідь вірна')
         _bal=_bal+1
     elif(cosmos==9):
         print('Якщо враховувати плутон тоді так, але плутон вже не рахуєтся як планета')
         _bal=_bal+1
-    elif(cosmos==1 or cosmos==4):
+    elif(plenet==1 or plenet==4):
         print('Віповідь не вірна')
     else:
         print('Я вас не розумію')
     print('Скільки супутників має сатурн?')
     print('4   82   102   46')
-    cosmos=int(input())
-    if(cosmos==82):
+    saturn=int(input())
+    if(saturn==82):
         print('Відповідь вірна') 
         _bal=_bal+3
-    elif(cosmos==4 or cosmos==102 or cosmos==46):
+    elif(saturn==4 or saturn==102 or saturn==46):
         print('Відповідь не вірна')
     else:
         print('Я вас не розумію')
     print('Скільки карликових планет в сонячній системі?')
     print('1  17  9  5')
-    cosmos=int(input())
-    if(cosmos==5):
+    smallplenet=int(input())
+    if(smallplenet==5):
         print('Відповідь вірна') 
         _bal=_bal+2
-    elif(cosmos==17 or cosmos==9 or cosmos==1):
+    elif(smallplenet==17 or smallplenet==9 or smallplenet==1):
         print('Відповідь не вірна')
     else:
         print('Я вас не розумію')
     print('Скільки планет сонячної системи мають кільця?')
     print('1  6  4  3')
-    cosmos=int(input())
-    if(cosmos==4):
+    rings=int(input())
+    if(rings==4):
         print('Відповідь вірна') 
         _bal=_bal+2
-    elif(cosmos==3 or cosmos==6 or cosmos==1):
+    elif(rings==3 or rings==6 or rings==1):
         print('Відповідь не вірна')
     else:
         print('Я вас не розумію')
     print('Скільки кілець має Уран?')
     print('13  18  100  78')
-    cosmos=int(input())
-    if(cosmos==13):
+    uran=int(input())
+    if(uran==13):
         print('Відповідь вірна') 
         _bal=_bal+3
-    elif(cosmos==14 or cosmos<=14 ):
+    elif(uran==14 or uran<=14 ):
         print('Відповідь не вірна')
     else:
         print('Я вас не розумію')
---
+++ WhileList.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+
+classmates=['Влад', 'Саша', 'Максим', 'Рома', 'Андрій', 'Юра', 'Артем', 'Вова' ]
+clas=0
+name=0
+while(clas<8):
+    clas=clas+1
+    print(classmates[name])
+    name=name+1
+    
