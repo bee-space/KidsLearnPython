fde4b990554c57ad34d2d7336873af9d723984c1
misha-tovstenko <tovstenko.misha@gmail.com>
2023-08-23 11:01:53 +0300

    DictionaryTanks

---
+++ DictionaryTanks.py
@@ -0,0 +1,21 @@
+
+tank={'T-26''Panzer III''M2''Churchill I''Char B1''Strv m/42'}
+tankcaliber={'T-26':'45-мм','Panzer III':'37-мм','M2':'37-мм','Tetrarch':'40-мм','Char B1':'75-мм, 47-мм','Strv m/41':'37-мм'}
+tankSpeed={'T-26':'30 км/ч','Panzer III':'35 км/ч','M2':'43 км/ч','Tetrarch':'64 км/ч','Char B1':'28 км/ч','Strv m/41':'43 км/ч'}
+tankCrew={'T-26':'3','Panzer III':'5','M2':'6','Tetrarch':'3','Char B1':'4','Strv m/42':'4',}
+
+print('Калібр танка Т-26 '+tankcaliber['T-26'])
+print('Швидеість по шосе танка Strv m/41 '+tankSpeed['Strv m/41'])
+print('Екіпаж танку Tetrarch '+tankCrew['Tetrarch'])
+
+#Я не зробив базу данних про однокласників томущо я не знав в них стільки інформації.
+#Але я зробив базу данних про танки.
+#Це що я зробив це ще не все я хочу сюди ще багато чого добавити. 
+
+
+
+
+
+
+
+
